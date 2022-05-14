3774e4ff068362465c6ce288e724d93715cf7101
Boris <boris@randebrock.com>
2022-05-14 15:31:51 +0200

    magically solved- nobody knows why

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 require("dotenv").config();
+const chalk = require("chalk");
+const debug = require("debug")("robots:root");
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const port = process.env.SERVER_PORT || 5000;
+
+const server = app.listen(port, () => {
+  debug(chalk.greenBright(`Server listening on ${port}`));
+
+  server.on("error", (error) => {
+    debug(chalk.redBright("all wrong"));
+    if (error.code === "EADDRINUSE") {
+      debug(chalk.red("port in use"));
+    }
+  });
+});
+
+app.use(morgan("dev"));
+app.use(express.json());
