c68efb28a045a179b9d001ed8083e85e5c8273cb
Boris <boris@randebrock.com>
2022-05-15 18:48:47 +0200

    added controlers and tried to test..

---
+++ server/mocks/robots.js
@@ -0,0 +1,21 @@
+const robots = [
+  {
+    speed: 7,
+    name: "Bilbo2",
+    creationDate: 2032003,
+    age: 21,
+  },
+  {
+    speed: 6,
+    name: "Bilboa",
+    creationDate: 2032003,
+    age: 211,
+  },
+  {
+    speed: 9,
+    name: "Balbo",
+    creationDate: 2032003,
+    age: 25,
+  },
+];
+module.exports = { robots };
