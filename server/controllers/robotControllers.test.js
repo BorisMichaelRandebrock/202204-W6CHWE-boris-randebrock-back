3c3cf3e9a6f66e71d45dcc578723ca8c9e70f6ae
Boris <boris@randebrock.com>
2022-05-17 08:31:29 +0200

    git is nervous...

--- server/controllers/robotControllers.test.js
+++ server/controllers/robotControllers.test.js
@@ -1,27 +1,27 @@
-// require("dotenv").config();
-// const { robots } = require("../mocks/robots");
-// const { getRobots } = require("./robotControllers");
+require("dotenv").config();
+const { robots } = require("../mocks/robots");
+const { getRobots } = require("./robotControllers");
 
-// jest.mock("../../db/models/robots", () => ({
-//   ...jest.requireActual("../../db/models/robots"),
-//   find: () => robots,
-//   findById: () => robots,
-// }));
+jest.mock("../../db/models/robots", () => ({
+  ...jest.requireActual("../../db/models/robots"),
+  find: () => robots,
+  findById: () => robots,
+}));
 
-// describe("Given a getRobots function", () => {
-//   const req = jest.fn();
-//   const res = {
-//     status: jest.fn().mockReturnThis(),
-//     json: jest.fn(),
-//   };
+describe("Given a getRobots function", () => {
+  const req = jest.fn();
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
 
-//   describe("When invoked", () => {
-//     test("Then it sould call response's status method with value 200", async () => {
-//       const expectStatusValue = 200;
+  describe("When invoked", () => {
+    test("Then it sould call response's status method with value 200", async () => {
+      const expectStatusValue = 200;
 
-//       await getRobots(req, res);
+      await getRobots(req, res);
 
-//       expect(res.status).toHaveBeenCalledWith(expectStatusValue);
-//     });
-//   });
-// });
+      expect(res.status).toHaveBeenCalledWith(expectStatusValue);
+    });
+  });
+});
